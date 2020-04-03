cd14b39001e76066a860c72047a44c578ac4707e
ameen4455 <ameen4455@gmail.com>
2020-04-03 10:39:26 +0530

    added graph

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -2,11 +2,13 @@ import './chart.scss';
 import React, {useEffect, useRef} from 'react';
 import Chart from 'chart.js';
 
-function Charting(){
+const Charting = (props) => {
     const canvasRef = useRef();
     useEffect(() => {
         new Chart(canvasRef.current.getContext("2d"), {
             type: "pie",
+            responsive: true,
+            maintainAspectRatio: false,
             data: {
                 //Bring in data
                 labels: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
@@ -14,7 +16,15 @@ function Charting(){
                     {
                         backgroundColor: ['#fa7f70', '#f8433f', '#7b1d1b', '#ef403d', '#ce5c5c', '#c33230', '#e1455e', '#b22d2b'],
                         label: "Sales",
-                        data: [86, 67, 91, 45, 55, 88, 25, 66],
+                        data: [props.data['A+ve'] ? props.data['A+ve'] : 0,
+                            props.data['A-ve'] ? props.data['A-ve'] : 0,
+                            props.data['B+ve'] ? props.data['B+ve'] : 0,
+                            props.data['B-ve'] ? props.data['B-ve'] : 0,
+                            props.data['AB+ve'] ? props.data['AB+ve'] : 0,
+                            props.data['AB-ve'] ? props.data['AB-ve'] : 0,
+                            props.data['O+ve'] ? props.data['O+ve'] : 0,
+                            props.data['O-ve'] ? props.data['O-ve'] : 0,
+                        ],
                     }
                 ]
             },
@@ -26,7 +36,7 @@ function Charting(){
 
     return(
         <div id="chdiv">
-            <canvas id="blchart" ref={canvasRef} />
+            <canvas id="blchart"  ref={canvasRef} />
         </div>
     );
 }
--- src/pages/Home/BloodList/BloodList.js
+++ src/pages/Home/BloodList/BloodList.js
@@ -1,61 +1,100 @@
 import React, { useEffect, useState } from 'react';
-import "./BLoodList.scss"
-import {Col,Row,Container} from 'react-bootstrap';
+import './BLoodList.scss';
+import { Col, Row, Container, Collapse } from 'react-bootstrap';
 import { useAuth } from '../../../context/auth';
 import Heart from '../../../assets/heartBeat.svg';
+import Charting from '../../../components/chart/Chart';
 
 const BloodList = () => {
   const value = useAuth();
 
-  const [data,setData] = useState([]);
+  const phoneCall = n => {
+    window.location.href = 'tel:' + n;
+  };
 
-  useEffect(()=>{
+  const email = n => {
+    window.location.href = 'mailto:' + n;
+  };
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
     setData([]);
-    fetch(value.domain +"/search-availability/?blood_group="+value.group+"&taluk="+value.thaluk)
+    fetch(
+      value.domain +
+        '/search-availability/?blood_group=' +
+        value.group +
+        '&taluk=' +
+        value.thaluk,
+    )
       .then(res => res.json())
-      .then(res => setData(res))
-  },[value.thaluk,value.district]);
+      .then(res => setData(res));
+  }, [value.thaluk, value.district]);
 
-  return(
+  return (
     <>
-      {data.length?
+      {data.length ? (
         <Container className="cardContainer">
-          {
-            data.map(data => {
-                return (
-                  <Row>
-                    <Col lg={{ span: 4, offset: 4 }}>
-                      <div className="cardBod">
-                        <div className="cardVal">
-                          <p>{data.blood_group_count}</p>
-                        </div>
-                        <div className="cardInfo">
-                          <div className="cardCollegeName">
-                            {data.name}
-                          </div>
-                          <div className="cardAddress">
-                            {data.location}
-                          </div>
-                          <div className="cardDoner">
-                            Doners of <span style={{ fontWeight: 500 }}>{data.requested_blood_group}</span> available
-                          </div>
-                        </div>
+          {data.map(data => {
+            return (
+              <Row className={"mt-3"}>
+                <Col lg={{ span: 6, offset: 3 }}>
+                  <Col className="cardBod pt-2 d-flex flex-column">
+                    <Row>
+                      <div className="d-flex cardVal">
+                        <div className={"align-self-center m-2 ml-3 pt-1"}>{data.blood_group_count}</div>
                       </div>
-                    </Col>
-                  </Row>
-                )
-              }
-            )
-          }
+                      <Col className="cardInfo">
+                        <div className="cardCollegeName">{data.name}</div>
+                        <div className="cardAddress">{data.location}</div>
+                        <div className="cardDoner">
+                          Doners of {" "}
+                          <span style={{ fontWeight: 500 }}>
+                            {data.requested_blood_group}
+                          </span>{' '}
+                          available
+                        </div>
+                      </Col>
+                    </Row>
+                    <Row className={"mt-2 mb-2"}>
+                      <Col>
+                        <Charting data={{'B+ve':5,'A-ve':6,'A+':1}}/>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className={'poc m-2'}>
+                        <div className={'poc-head font-weight-medium'}>
+                          Point of Contact
+                        </div>
+                        <div className={'poc-body mt-1 position-relative'}>
+                          <div className={'poc-name'}>Abin</div>
+                          <div
+                            className={'poc-number'}
+                            onClick={() => phoneCall('+919633288284')}
+                          >
+                            +919633288284
+                          </div>
+                          <div
+                            className={'poc-email'}
+                            onClick={() => email('ameen4455@gmail.com')}
+                          >
+                            ameen4455@gmail.com
+                          </div>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Col>
+              </Row>
+            );
+          })}
         </Container>
-        :
-        <div className={
-          "heartLoader"
-        }>
-          <img src={Heart} alt=""/>
+      ) : (
+        <div className={'heartLoader'}>
+          <img src={Heart} alt="" />
         </div>
-      }
+      )}
     </>
-  )
+  );
 };
 export default BloodList;
